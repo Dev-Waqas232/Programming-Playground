80bcaea389893e1c1ce71f9752704dd1e3053fa1
Waqas Munir <devwaqas232@gmail.com>
2024-12-02 13:13:38 +0500

    Learnt About Services, Repositories & Dependency Injection

---
+++ 02-Frameworks & Libraries/nest-js-course/02-messages/src/messages/messages.repository.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { readFile, writeFile } from 'fs/promises';
+
+@Injectable()
+export class MessageRepository {
+  async findOne(id: string) {
+    const content = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(content);
+    return messages[id];
+  }
+
+  async findAll() {
+    const content = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(content);
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
